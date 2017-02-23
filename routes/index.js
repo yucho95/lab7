8ca096916a3b94947ff3a43e288c7f3335560fe6
Yubin Cho <yucho@ucsd.edu>
2017-02-23 10:36:12 -0800

    add

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
-exports.view = function(req, res){
-  	res.render('index', projects);
-  	projects["min"] = false;
-};
 exports.viewMin = function(req, res){
 	res.render('index', projects);
 	projects["min"] = true;
+};
+exports.view = function(req, res){
+  	res.render('index', projects);
+  	projects["min"] = false;
 };
