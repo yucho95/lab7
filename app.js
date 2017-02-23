392ec8ebd464c648b2ae9d8f332509ca48f0911b
Yubin Cho <yucho@ucsd.edu>
2017-02-23 10:25:17 -0800

    change

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var project = require('./routes/project');
+var control = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
 
@@ -38,6 +39,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.view);
+app.get('/min', index.viewMin);
 // Example route
 // app.get('/users', user.list);
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ var projects = require('../projects.json');
 
 exports.view = function(req, res){
   	res.render('index', projects);
+  	projects["min"] = false;
+};
+exports.viewMin = function(req, res){
+	res.render('index', projects);
+	projects["min"] = true;
 };
